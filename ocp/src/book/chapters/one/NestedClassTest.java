f500bcffa8f4ba87eb5c4781ed3fb235f3716da7
kbrv37 <apizano@purdue.edu>
2021-01-20 22:07:48 -0600

    nested classes OCP

---
+++ ocp/src/book/chapters/one/NestedClassTest.java
@@ -0,0 +1,26 @@
+package book.chapters.one;
+
+public class NestedClassTest {
+    private static int x = 5;
+
+    static class Inner {
+        // will mask variable if declared here or take the parent variable with same name
+        //public static int x = 10;
+
+        public static void go() {
+            System.out.println(x);
+        }
+    }
+
+    Inner inner = new Inner();
+
+    public void doSomething() {
+        inner.go();
+    }
+
+    public static void main(String[] args) {
+        NestedClassTest nestedClassTest = new NestedClassTest();
+        //Inner inner = nestedClassTest.new Inner();
+        NestedClassTest.Inner.go();
+    }
+}
